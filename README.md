a6b14ad3ed8cadf807815e2561d94fef245e5d72
KnewHow <948170910@qq.com>
2018-10-03 18:52:58 +0800

    fix build status

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-![![Build Status](https://travis-ci.org/KnewHow/studyFPScala.svg?branch=master)](https://travis-ci.org/KnewHow/studyFPScala)
+[![Build Status](https://travis-ci.org/KnewHow/studyFPScala.svg?branch=master)](https://travis-ci.org/KnewHow/studyFPScala)
+
 Study FP Scala node and code
--- src/main/scala/parsing/Parsers.scala
+++ src/main/scala/parsing/Parsers.scala
@@ -2,7 +2,12 @@ package fpscala.parsing
 
 sealed trait Parser[PaserError, Parser[+ _]] {
   self =>
+  def run[A](p: Parser[A])(input: String): Either[PaserError, A]
+  def char[A](c: Char): Parser[Char]
   def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+  def map[A, B](f: A => B): Parser[B]
+  def flatMap[A, B](f: A => Parser[B]): Parser[B]
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps(p)
 
