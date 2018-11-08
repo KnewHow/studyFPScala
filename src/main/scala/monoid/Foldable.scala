542cd70e176e4872067d098d7906c6f242e49aca
KnewHow <948170910@qq.com>
2018-11-08 19:50:33 +0800

    implement tree foldable

--- src/main/scala/monoid/Foldable.scala
+++ src/main/scala/monoid/Foldable.scala
@@ -5,6 +5,7 @@ trait Foldable[F[_]] {
   def foldRight[A, B](as: F[A], z: B)(f: (A, B) => B): B
   def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B
   def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as, m.zero)(m.op)
+  def toList[A](fa: F[A]): List[A]              = foldRight(fa, List[A]())(_ :: _)
 }
 
 case object ListFoldable extends Foldable[List] {
@@ -30,4 +31,39 @@ case object StreamFoldable extends Foldable[Stream] {
     as.foldRight(z)(f)
   def foldMap[A, B](as: Stream[A])(f: A => B)(mb: Monoid[B]): B =
     as.foldLeft(mb.zero)((b, a) => mb.op(b, f(a)))
+}
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A)                        extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+case object TreeFoldable extends Foldable[Tree] {
+  def foldLeft[A, B](as: Tree[A], z: B)(f: (B, A) => B): B = as match {
+    case Leaf(v)      => f(z, v)
+    case Branch(l, r) => foldLeft(r, (foldLeft(l, z)(f)))(f)
+  }
+  def foldRight[A, B](as: Tree[A], z: B)(f: (A, B) => B): B = as match {
+    case Leaf(v)      => f(v, z)
+    case Branch(l, r) => foldRight(l, foldRight(r, z)(f))(f)
+
+  }
+  def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B = as match {
+    case Leaf(v)      => mb.zero
+    case Branch(l, r) => mb.op(foldMap(l)(f)(mb), foldMap(r)(f)(mb))
+  }
+}
+
+case object OptionFoldable extends Foldable[Option] {
+  def foldLeft[A, B](as: Option[A], z: B)(f: (B, A) => B): B = as match {
+    case None    => z
+    case Some(a) => f(z, a)
+  }
+  def foldRight[A, B](as: Option[A], z: B)(f: (A, B) => B): B = as match {
+    case None    => z
+    case Some(a) => f(a, z)
+  }
+  def foldMap[A, B](as: Option[A])(f: A => B)(mb: Monoid[B]): B = as match {
+    case None    => mb.zero
+    case Some(a) => f(a)
+  }
 }
